2ef1ca835ec37f03ea19809c28d53a02b6807e86
Daniel Beleza <danielbeleza@gmail.com>
2021-06-15 23:39:50 +0100

    - created demo module and added compose dependencies to it and also dependency to "app" module - updated existing gradle dependencies versions

---
+++ demo/src/main/java/com/atoguitar/demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.atoguitar.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
