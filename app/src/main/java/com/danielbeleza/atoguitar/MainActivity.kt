22e521ba0ab0d45396d276c4a7c18837c69a2437
Daniel Beleza <danielbeleza@gmail.com>
2021-06-15 23:21:09 +0100

    - added remaining guitar chords to ChordFactory, at least the most important ones

--- app/src/main/java/com/danielbeleza/atoguitar/MainActivity.kt
+++ app/src/main/java/com/danielbeleza/atoguitar/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val chord = ChordFactory.buildChordFromLetter("B")
+            val chord = ChordFactory.buildChordFromLetter("Bb")
             GuitarStringsLayout(chord)
         }
     }
